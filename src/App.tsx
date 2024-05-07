b0e36351f7fa46a5a5d6dce006b5ddd3e1c0560a
Anatolii Chakkaev <ac@ub.io>
2024-05-07 19:01:48 +0100

    Test focus capture

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import './App.css';
 import { JsonViewer } from './JsonViewer.js';
+import { useState } from 'react';
 import data from './MOCK_DATA.json';
 import repos from './GITHUB_REPOS.json';
 import schema from './schema.json';
 
 function App() {
+    const [name, setName] = useState('');
+
     return (
         <>
             <JsonViewer>{ JSON.stringify(data) }</JsonViewer>
             <JsonViewer>{ JSON.stringify(repos) }</JsonViewer>
             <JsonViewer>{ JSON.stringify(schema) }</JsonViewer>
+            <input value={ name } onInput={ (e) => setName(e.target.value) } />
+            <JsonViewer>{ JSON.stringify({ name }) }</JsonViewer>
         </>
-    )
+    );
 }
 
 export default App;
